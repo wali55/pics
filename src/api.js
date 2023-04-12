74bd85bc1b944b5e9197814836310d69855e56a1
wali55 <walisantunu@gmail.com>
2023-04-13 00:15:01 +0600

    HTTP request

--- src/api.js
+++ src/api.js
@@ -1,1 +1,18 @@
 import axios from 'axios';
+
+const searchImages = async () => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+        headers: {
+            Authorization: 'Client-ID Y2TIMAwoEQBVpxvp7XmFS0CXEemwtTkG4Bdme_wpXi4'
+        },
+        params: {
+            query: 'cars'
+        }
+    });
+
+    console.log(response)
+
+    return response;
+};
+
+export default searchImages;
