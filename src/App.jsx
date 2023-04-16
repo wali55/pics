0abe33e03aebc16f52a5b224dd0c638788944158
wali55 <walisantunu@gmail.com>
2023-04-16 23:37:44 +0600

    display list

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import SearchBar from "./components/SearchBar";
+import ImageList from "./components/ImageList";
 import searchImages from "./api";
 
 function App() {
-    const handleSubmit = async (term) => {
-        const result = await searchImages(term);
-        
-        console.log(result)
-    };
+  const [images, setImages] = useState([]);
+
+  const handleSubmit = async (term) => {
+    const result = await searchImages(term);
+
+    setImages(result);
+  };
 
   return (
     <div>
       <SearchBar onSubmit={handleSubmit} />
+      <ImageList images={images} />
     </div>
   );
 }
